5c5ea160c5fd44bdce397f7499d306260f6542ec
GuilhermSA07 <Guilherm.Araujo.Silva@gmail.com>
2019-07-03 17:02:15 -0300

    adicionar - javascript

---
+++ Se e Senão.js
@@ -0,0 +1,9 @@
+var i = tchau;
+var oi = "Bom dia meu consagrado";
+var tchau = "Boa noite meu avenger";
+
+if (i == oi){
+    console.log(oi);
+} else {
+    console.log(tchau);
+}
---
+++ testezinho de vetor.js
@@ -0,0 +1,10 @@
+//vetor
+var langs = ["html","css","javascript"]
+//vetor chamando o próprio vetor e a função length
+//essa função acrescenta
+langs [langs.length] = "php"
+//imprimir a função
+var tam = langs.length;
+var localizacao = langs[0]
+console.log(localizacao)
+console.log(tam)
